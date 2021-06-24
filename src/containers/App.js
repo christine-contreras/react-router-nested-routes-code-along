ac2ea8b9f43db26a1a14e7462442a96edceeacfd
Christine Contreras <christineec23@yahoo.com>
2021-06-24 09:49:12 -0500

    finished

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,7 +21,9 @@ class App extends Component {
       <Router>
         <div>
           <NavBar />
+          {/* Aside: Notice that instead of the component prop, we're using render. > The render prop works very similarly to component, but instead of passing > an entire component in, we must pass a function that returns JSX. */}
           <Route exact path="/" render={() => <div>Home</div>} />
+          {/* the Route will call the function inside render and pass in the current information available about the route, including the URL path that caused the Route to render. This is not possible with the regular component prop on Routes we've seen before. */}
           <Route path='/movies' render={routerProps => <MoviesPage {...routerProps} movies={this.state.movies}/>} />
         </div>
       </Router>
